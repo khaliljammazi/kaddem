b5106afc62fa5f604310e9814d9a175198e4cc0d
khalil.jammezi <khalil.jammezi@keyrus.com>
2023-02-15 09:46:23 +0100

    modify repo

--- src/main/java/com/esprit/kaddem/repositories/EquipeRepository.java
+++ src/main/java/com/esprit/kaddem/repositories/EquipeRepository.java
@@ -4,4 +4,5 @@ import com.esprit.kaddem.entites.Equipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EquipeRepository  extends JpaRepository<Equipe,Integer> {
+
 }
--- src/main/java/com/esprit/kaddem/repositories/EtudiantRepository.java
+++ src/main/java/com/esprit/kaddem/repositories/EtudiantRepository.java
@@ -1,12 +1,13 @@
 package com.esprit.kaddem.repositories;
 
 import com.esprit.kaddem.entites.Etudiant;
+import com.esprit.kaddem.entites.Niveau;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EtudiantRepository extends JpaRepository<Etudiant,Integer> {
-    List<Etudiant> findEtudiantByDepartment_IdDepartment(Integer departmentId);
+    List<Etudiant> findByDepartmentIdDepartment(long idDepartment);
 
-    List<Etudiant> findByEquipes_Niveau_Expert();
+    List<Etudiant> findByEquipesNiveau(Niveau niveau);
 }
